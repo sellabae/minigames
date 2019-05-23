9e15cff3ee8ffe775d2137fae01239892e4786c0
sellabae <devsellabae@gmail.com>
2019-05-22 23:59:17 -0700

    edit shuffle effect and restart

--- memory-game/scripts.js
+++ memory-game/scripts.js
@@ -8,21 +8,27 @@ let lockBoard = false;
 let firstCard, secondCard;
 const cardFlipSound = document.getElementById('cardFlipAudio');
 const cardShuffleSound = document.getElementById('cardShuffleAudio');
+let time=0, move=0, matches=0;
 
 //this function can be invoked right after its declaration by using
 //Immediately Invoked Function Expression (IIFE): Self-Executing Anonymous Function
 //e.g. (function func_name(){...})();
 function startGame() {
-  //attach event listener to all cards
-  //element.addEventListener(event, function, useCapture)
-  cards.forEach(card => card.addEventListener('click', flipCard));
-  shuffle();
-  //change button text to 'RESTART'
-  document.getElementById('startBtn').innerText = 'RESTART';
+  console.log('initialize the game');
   //unflip all cards
   cards.forEach(card => {
     card.classList.remove('flip');
   });
+  resetBoard();
+  time = 0;
+  move = 0;
+  //shuffle cards
+  shuffle();
+  //attach event listener to all cards
+  //element.addEventListener(event, function, useCapture)
+  cards.forEach(card => card.addEventListener('click', flipCard));
+  //change button text to 'RESTART'
+  document.getElementById('startBtn').innerText = 'RESTART';
 };
 
 //call flip action
@@ -31,6 +37,8 @@ function flipCard() {
   if (lockBoard) return;
   //prevent same card click
   if(this === firstCard) return;
+  move++;
+  updateMessageBoard();
 
   //flip the card
   this.classList.add('flip');
@@ -60,7 +68,10 @@ function checkForMatch() {
 
 function matchAction() {
   console.log('Match!');
-
+  matches++;
+  if(matches == 6){
+    console.log('You Win! move:'+move+' time:'+time);
+  }
   disableCards();
 }
 
@@ -78,7 +89,7 @@ function unflipCards() {
     secondCard.classList.remove('flip');
 
     resetBoard();
-  }, 1500);
+  }, 1000);
 }
 
 function resetBoard() {
@@ -88,35 +99,50 @@ function resetBoard() {
 }
 
 function shuffle() {
-  //shuffle sound effect
-  cardShuffleSound.currentTime = 0;
-  let promise = cardShuffleSound.play();
-  if(promise !== undefined) {
-    promise.then(_ => {
-      //Autoplay started.
-      console.log('shuffle sound played.');
-    }).catch(error => {
-      //Autoplay was prevented.
-      console.log('shuffle sound autoplay blocked.');
-      // Show a "Play" button so that user can start playback.
-    });
-  }
-
-  //spin animation
-  cards.forEach(card => {
-    //restart spin animation
-    card.classList.remove('spin');
-    void card.offsetWidth;
-    card.classList.add('spin');
-    //TODO: how to delay between iteration??
-    //...
-  });
-  console.log('shuffle effect');
-
+  shuffleEffect();
   //randomizing card order
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
   });
   console.log('shuffled the cards.');
+}
+
+function shuffleEffect() {
+    //shuffle sound effect
+    cardShuffleSound.currentTime = 0;
+    let promise = cardShuffleSound.play();
+    if(promise !== undefined) {
+      promise.then(_ => {
+        //Autoplay started.
+        console.log('shuffle sound played.');
+      }).catch(error => {
+        //Autoplay was prevented.
+        console.log('shuffle sound autoplay blocked.');
+        // Show a "Play" button so that user can start playback.
+      });
+    }
+    //spin animation with delay between iteration
+    for (var i=0; i<cards.length; i++) {
+      (function (i) {
+        setTimeout(function () {
+          //restart spin animation
+          cards[i].classList.remove('spin');
+          void cards[i].offsetWidth;
+          cards[i].classList.add('spin');
+        }, 50*i);
+      })(i);
+    }
+    // cards.forEach(card => {
+    //   card.classList.remove('spin');
+    //   void card.offsetWidth;
+    //   card.classList.add('spin');
+    //   //TODO: how to delay between iteration??
+    //   //...
+    // });
+}
+
+function updateMessageBoard() {
+  document.getElementById('moveValue').innerText = move;
+  document.getElementById('timeValue').innerText = time;
 }
